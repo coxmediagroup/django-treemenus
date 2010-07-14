dc0ce41c1596e358f0190c2e650c2e525371b266
James Hsiao <james.hsiao@cmgdigital.com>
2010-07-14 17:20:40 -0400

    clone of treemenus at 0.8.6

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-treemenus',
+    version='0.8.6',
+    description='Tree-structured menuing application for Django.',
+    author='Julien Phalip',
+    author_email='julien@julienphalip.com',
+    url='http://code.google.com/p/django-treemenus/',
+    packages=find_packages(),
+    package_data={
+        'treemenus': [
+            'templates/admin/treemenus/menu/*.html',
+            'templates/admin/treemenus/menuitem/*.html'
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
